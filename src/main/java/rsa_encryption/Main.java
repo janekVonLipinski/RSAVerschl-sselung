f631171f0ef48e30d8a783b07fc8f67f9d917d99
janekVonLipinski <janekvonlipinski222@gmail.com>
2025-06-25 10:54:37 +0200

    added code to main

--- src/main/java/rsa_encryption/Main.java
+++ src/main/java/rsa_encryption/Main.java
@@ -1,6 +1,21 @@
 package rsa_encryption;
 
+import rsa_encryption.encryption.RSA;
+
 public class Main {
 
+    public static void main(String[] args) {
 
+        RSA rsa = new RSA();
+
+        rsa.generateKeyPair();
+        int message = 8;
+        System.out.println("Message is " + message);
+
+        int encryptedMessage = rsa.encrypt(message);
+        System.out.println("Encrypted is " + encryptedMessage);
+
+        int decryptedMessage = rsa.decrypt(encryptedMessage);
+        System.out.println("Mesage is " + decryptedMessage);
+    }
 }
