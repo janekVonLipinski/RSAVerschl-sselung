80b52f11fcfa2045b68caffedf05a3857c8b2b36
LangeP <paulaaaalange@gmail.com>
2025-06-25 10:52:37 +0200

    getGreatestCommonDenominatorChecker implementation; test getGreatestCommonDenominatorChecker implementation

--- src/main/java/rsa_encryption/euclidic_algorithm/impl/EuclidicCommonDenominatorChecker.java
+++ src/main/java/rsa_encryption/euclidic_algorithm/impl/EuclidicCommonDenominatorChecker.java
@@ -5,6 +5,12 @@ import rsa_encryption.euclidic_algorithm.GreatestCommonDenominatorChecker;
 public class EuclidicCommonDenominatorChecker implements GreatestCommonDenominatorChecker {
     @Override
     public int getGreatestCommonDenomimnator(int p, int q) {
-        return 0;
+        while (q != 0){
+            int temp = q;
+            q = p % q;
+            p = temp;
+
+        }
+        return p;
     }
 }
---
+++ src/test/java/rsa_encryption/euclidic_algorithm/impl/EuclidicCommonDenominatorCheckerTest.java
@@ -0,0 +1,19 @@
+package rsa_encryption.euclidic_algorithm.impl;
+
+import org.junit.jupiter.api.Test;
+import rsa_encryption.euclidic_algorithm.GreatestCommonDenominatorChecker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EuclidicCommonDenominatorCheckerTest {
+
+    private final GreatestCommonDenominatorChecker sut = new EuclidicCommonDenominatorChecker();
+
+
+    @Test
+    void test() {
+    int res = sut.getGreatestCommonDenomimnator(12, 15);
+    assertEquals(3, res);
+    }
+
+}
