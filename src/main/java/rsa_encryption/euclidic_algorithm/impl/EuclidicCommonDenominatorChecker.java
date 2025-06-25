61aee6153679019203e667e2dd9acab87f284957
LangeP <paulaaaalange@gmail.com>
2025-06-25 10:22:56 +0200

    separated common denominator from multiplicative inverse

---
+++ src/main/java/rsa_encryption/euclidic_algorithm/impl/EuclidicCommonDenominatorChecker.java
@@ -0,0 +1,10 @@
+package rsa_encryption.euclidic_algorithm.impl;
+
+import rsa_encryption.euclidic_algorithm.GreatestCommonDenominatorChecker;
+
+public class EuclidicCommonDenominatorChecker implements GreatestCommonDenominatorChecker {
+    @Override
+    public int getGreatestCommonDenomimnator(int p, int q) {
+        return 0;
+    }
+}
